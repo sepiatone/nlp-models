7949355e239a5ab60dfbaf5af5dff0338de3b6d8
sepiatone <sepia_tone@protonmail.com>
2020-03-27 15:26:38 -0400

    Update util.py

--- src/util.py
+++ src/util.py
@@ -117,3 +117,24 @@ class MTDataset(data.Dataset):
               (self.max_trg_seq_length - trg_len))
 
     return torch.tensor(src_id), src_len, torch.tensor(trg_id), trg_len
+
+  
+class SimpleLossCompute:
+  """A simple loss compute and train function."""
+
+  def __init__(self, generator, criterion, opt = None):
+    self.generator = generator
+    self.criterion = criterion
+    self.opt = opt
+
+  def __call__(self, x, y, norm):
+    x = self.generator(x)
+    loss = self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1))
+    loss = loss / norm
+
+    if self.opt is not None:  # training mode
+      loss.backward()          
+      self.opt.step()
+      self.opt.zero_grad()
+
+    return loss.data.item() * norm
