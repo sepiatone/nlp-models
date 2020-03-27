f0a6dae90bc48745fa202f80ff6f3beedcb6e814
sepiatone <sepia_tone@protonmail.com>
2020-03-27 11:42:06 -0400

    Update util.py

--- src/util.py
+++ src/util.py
@@ -38,3 +38,63 @@ def read_sentence_file(filename):
 def read_vocab_file(filename):
   with open(filename, "r") as f:
     return [line.strip() for line in f]
+  
+  
+"""
+used to load the dataset. the class MTDataset is built on top of the data loader api provided by pytorch
+"""
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+
+
+# These IDs are reserved.
+PAD_INDEX = 0
+UNK_INDEX = 1
+SOS_INDEX = 2
+EOS_INDEX = 3
+
+
+class MTDataset(data.Dataset):
+  def __init__(self, src_sentences, src_vocabs, trg_sentences, trg_vocabs,
+               sampling=1.):
+    self.src_sentences = src_sentences[:int(len(src_sentences) * sampling)]
+    self.trg_sentences = trg_sentences[:int(len(src_sentences) * sampling)]
+
+    self.max_src_seq_length = MAX_SENT_LENGTH_PLUS_SOS_EOS
+    self.max_trg_seq_length = MAX_SENT_LENGTH_PLUS_SOS_EOS
+
+    self.src_vocabs = src_vocabs
+    self.trg_vocabs = trg_vocabs
+
+    self.src_v2id = {v : i for i, v in enumerate(src_vocabs)}
+    self.src_id2v = {val : key for key, val in self.src_v2id.items()}
+    self.trg_v2id = {v : i for i, v in enumerate(trg_vocabs)}
+    self.trg_id2v = {val : key for key, val in self.trg_v2id.items()}
+
+  def __len__(self):
+    return len(self.src_sentences)
+
+  def __getitem__(self, index):
+    src_sent = self.src_sentences[index]
+    src_len = len(src_sent) + 2   # add <s> and </s> to each sentence
+    src_id = []
+    for w in src_sent:
+      if w not in self.src_vocabs:
+        w = '<unk>'
+      src_id.append(self.src_v2id[w])
+    src_id = ([SOS_INDEX] + src_id + [EOS_INDEX] + [PAD_INDEX] *
+              (self.max_src_seq_length - src_len))
+
+    trg_sent = self.trg_sentences[index]
+    trg_len = len(trg_sent) + 2
+    trg_id = []
+    for w in trg_sent:
+      if w not in self.trg_vocabs:
+        w = '<unk>'
+      trg_id.append(self.trg_v2id[w])
+    trg_id = ([SOS_INDEX] + trg_id + [EOS_INDEX] + [PAD_INDEX] *
+              (self.max_trg_seq_length - trg_len))
+
+    return torch.tensor(src_id), src_len, torch.tensor(trg_id), trg_len
